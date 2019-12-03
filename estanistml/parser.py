eea540dbda25a92313447531b7386971407c29b2
MoacirMSJ <moacirmascarenha86@gmail.com>
2019-12-03 19:33:41 -0300

    Adicionado makefile alguns parsers e modificado questoes do docker

--- estanistml/parser.py
+++ estanistml/parser.py
@@ -5,10 +5,42 @@ from .runtime import Symbol
 
 class LispTransformer(InlineTransformer):
     def start(self, *args): 
-        return ["Macro", *args]
+        try:
+            return ["macro", *args]
+        except ValueError:
+            return ["import", *args]
+        
 
     def string(self, st):
-        return st[1:-1]  
+        res = str(st)[1:-1]
+        res = res.replace("\\n","\n").replace("\\t","\t").replace("\\","")
+        return res
+    
+    def numb(self, number):
+        try:
+            return int(number)
+        except ValueError:
+            return float(number)
+    
+    def args(self, *args):
+        return ("args", *args)
+    
+
+    # def atom(self, args):
+    #     print("aq")
+    #     try:
+    #         return self.numb(args)
+    #     except ValueError:
+    #         try:
+    #             res = str(args)[1:-1]
+    #             res = res.replace("\\n","\n").replace("\\t","\t").replace("\\","")
+    #             return res
+    #         except ValueError:
+    #             return list(args)
+
+
+    # def obj(self, *args):
+    #     return dict(*args)
    
 
     
